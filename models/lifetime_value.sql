04c4fbb1712135eed7893c5142b871c809a4e03c
PrekshChadha <51757021+PrekshChadha@users.noreply.github.com>
2025-10-17 21:55:00 +0530

    SQL

--- models/lifetime_value.sql
+++ models/lifetime_value.sql
@@ -1,33 +1,33 @@
 WITH customer_orders AS (
     SELECT
-        c.id AS customer_id,
-        c.first_name,
-        c.last_name,
-        MIN(o.order_date) AS first_order,
-        MAX(o.order_date) AS most_recent_order,
-        COUNT(o.id) AS number_of_orders
-    FROM {{ source('PUBLIC', 'raw_customers') }} c
-    LEFT JOIN {{ source('PUBLIC', 'raw_orders') }} o
-    ON c.id = o.user_id
-    GROUP BY c.id, c.first_name, c.last_name
+        cx.id AS customer_id,
+        cx.first_name,
+        cx.last_name,
+        MIN(ord.order_date) AS first_order,
+        MAX(ord.order_date) AS most_recent_order,
+        COUNT(ord.id) AS number_of_orders
+    FROM {{ source('PUBLIC', 'raw_customers') }} cx
+    LEFT JOIN {{ source('PUBLIC', 'raw_orders') }} ord
+    ON cx.id = ord.user_id
+    GROUP BY cx.id, cx.first_name, cx.last_name
 ),
 customer_lifetime_value AS (
     SELECT
-        o.user_id AS customer_id,
-        SUM(p.amount) AS customer_lifetime_value
-    FROM {{ source('PUBLIC', 'raw_orders') }} o
-    LEFT JOIN {{ source('PUBLIC', 'raw_payments') }} p
-    ON o.id = p.order_id
-    GROUP BY o.user_id
+        ord.user_id AS customer_id,
+        SUM(pyt.amount) AS customer_lifetime_value
+    FROM {{ source('PUBLIC', 'raw_orders') }} ord
+    LEFT JOIN {{ source('PUBLIC', 'raw_payments') }} pyt
+    ON ord.id = pyt.order_id
+    GROUP BY ord.user_id
 )
 SELECT
-    co.customer_id,
-    co.first_name,
-    co.last_name,
-    co.first_order,
-    co.most_recent_order,
-    co.number_of_orders,
-    clv.customer_lifetime_value
-FROM customer_orders co
-LEFT JOIN customer_lifetime_value clv
-ON co.customer_id = clv.customer_id
+    cxord.customer_id,
+    cxord.first_name,
+    cxord.last_name,
+    cxord.first_order,
+    cxord.most_recent_order,
+    cxord.number_of_orders,
+    cxlv.customer_lifetime_value
+FROM customer_orders cxord
+LEFT JOIN customer_lifetime_value cxlv
+ON cxord.customer_id = cxlv.customer_id
